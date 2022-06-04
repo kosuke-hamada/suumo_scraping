0e029b9dc3ce107c043d324b15d17657845cdcba
kosuke-hamada <hamada-kosuke@dropcode.jp>
2022-06-04 18:10:03 +0900

    福岡の物件情報を取得できるように改修

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ axios(chiba_ms_URL)
 
     $(".searchtable", htmlParser).each(function () {
       const aichi_itemlist = $(this).find(".searchitem-list-value").text().replace(/\)/g ,'').replace(/\(/g ,',').slice(1);
-      console.log("\n-----------愛知新築戸建て--------------/n")
+      console.log("\n-----------愛知新築戸建て--------------\n")
       var a = aichi_itemlist.split(',')
       var json_list = JSON.stringify(a)
       console.log(json_list)
@@ -155,5 +155,82 @@ axios(chiba_ms_URL)
 setTimeout(() => {
   aichi()
 }, 2000);
+
+function fukuoka(){
+  console.log("\n--------------\n\n福岡の物件情報\n\n--------------\n")
+
+  // 愛知の物件情報
+  const fukuoka_ms_URL = "https://suumo.jp/ms/chuko/fukuoka/city/";
+  const fukuoka_chukoikkodate_URL = "https://suumo.jp/chukoikkodate/fukuoka/city/";
+  const fukuoka_ikkodate_URL = "https://suumo.jp/ikkodate/fukuoka/city/";
+  const fukuoka_tochi_URL = "https://suumo.jp/tochi/fukuoka/city/";
+  
+  axios(fukuoka_ms_URL)
+  .then((response) => {
+    const htmlParser = response.data;
+    // console.log(htmlParser);
+    const $ = cheerio.load(htmlParser);
+
+    $(".searchtable", htmlParser).each(function () { 
+      //const list_count = $(this).find(".searchitem-list-value").length;
+
+      const fukuoka_itemlist = $(this).find(".searchitem-list-value").text().replace(/\)/g ,'').replace(/\(/g ,',').slice(1);
+      console.log("\n-----------福岡中古マンション--------------\n")
+  
+      var a = fukuoka_itemlist.split(',')
+      var json_list = JSON.stringify(a)
+      console.log(json_list)
+    });
+  })
+
+  axios(fukuoka_chukoikkodate_URL)
+  .then((response) => {
+    const htmlParser = response.data;
+    // console.log(htmlParser);
+    const $ = cheerio.load(htmlParser);
+
+    $(".searchtable", htmlParser).each(function () {
+        const fukuoka_itemlist = $(this).find(".searchitem-list-value").text().replace(/\)/g ,'').replace(/\(/g ,',').slice(1);
+        console.log("\n-----------福岡中古戸建て--------------\n")
+        var a = fukuoka_itemlist.split(',')
+        var json_list = JSON.stringify(a)
+        console.log(json_list)
+    });
+  })
+
+  axios(fukuoka_tochi_URL)
+  .then((response) => {
+    const htmlParser = response.data;
+    // console.log(htmlParser);
+    const $ = cheerio.load(htmlParser);
+
+    $(".searchtable", htmlParser).each(function () {
+      const fukuoka_itemlist = $(this).find(".searchitem-list-value").text().replace(/\)/g ,'').replace(/\(/g ,',').slice(1);
+      console.log("\n-----------福岡土地--------------\n")
+      var a = fukuoka_itemlist.split(',')
+      var json_list = JSON.stringify(a)
+      console.log(json_list)
+    });
+  })
+
+  axios(fukuoka_ikkodate_URL)
+  .then((response) => {
+    const htmlParser = response.data;
+    // console.log(htmlParser);
+    const $ = cheerio.load(htmlParser);
+
+    $(".searchtable", htmlParser).each(function () {
+      const fukuoka_itemlist = $(this).find(".searchitem-list-value").text().replace(/\)/g ,'').replace(/\(/g ,',').slice(1);
+      console.log("\n-----------福岡新築戸建て--------------\n")
+      var a = fukuoka_itemlist.split(',')
+      var json_list = JSON.stringify(a)
+      console.log(json_list)
+    });
+  })
+}
+
+setTimeout(() => {
+  fukuoka()
+}, 3000);
 
 //.catch((error) => console.log(error));
